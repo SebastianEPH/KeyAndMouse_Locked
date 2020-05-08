315aa0d79ea2c340fba6d3980b88b78960def38b
SebastiánEPH <sebastianeph99@gmail.com>
2020-05-08 17:46:37 -0500

    Other fixed

--- KeyAndMouseBlock.py
+++ KeyAndMouseBlock.py
@@ -1,20 +1,25 @@
+# v1.0
+
 from pynput.mouse import Button, Controller  # Importa librería Mouse
 import pythoncom, pyHook 
 
+def Block():
+    mouse = Controller()  
+    def BlockMouse():
+        mouse.position = (0, 0) # el mouse se va a la posición 0,0 de la pantalla
+        #mouse.press(Button.right)
+        #mouse.release(Button.right)
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+        
+    k = pyHook.HookManager()
+    while(True):
+        def e(event):
+            return False
+        BlockMouse()                # Bloquea el mouse
+        k.KeyAll = e
+        k.HookKeyboard()
+        pythoncom.PumpMessages()    # Bloquea Teclado
 
-mouse = Controller()  
-
-def BlockMouse():
-    mouse.position = (0, 0) # el mouse se va a la posición 0,0 de la pantalla
-    #mouse.press(Button.right)
-    #mouse.release(Button.right)
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-    
-k = pyHook.HookManager()
-while(True):
-    def e(event):
-        return False
-    k.KeyAll = e
-    k.HookKeyboard()
-    pythoncom.PumpMessages()
+#Inicia Script
+Block()
