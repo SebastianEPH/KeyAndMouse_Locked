5214f9be0fe9ffa66f0d9bf5314c716eb69f91f2
SebastianEPH <sebastianeph99@gmail.com>
2022-04-02 20:17:17 -0500

    documentation update

---
+++ install requirements.bat
@@ -0,0 +1,13 @@
+echo off
+color 05
+
+pip install pynput==1.6.8
+pip install pynput==1.6.8
+
+echo.
+echo.
+echo.
+echo.
+echo NOTA: Si tuvo algún error, trate de instalar las librerías manualmente
+:cmd
+pause null 
